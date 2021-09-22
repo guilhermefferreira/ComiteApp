b4781b6eecaadd2a7162a3e9fb3650504af0d254
Mateus Tomi <mtomi@itelinfo.com>
2021-09-22 15:38:10 -0400

    Atleta Repositorio criado

---
+++ src/main/java/com/comite/ComiteApp/repositories/AtletaRepository.java
@@ -0,0 +1,10 @@
+package com.comite.ComiteApp.repositories;
+
+import com.comite.ComiteApp.entities.Atleta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AtletaRepository extends JpaRepository <Atleta, Integer>{
+
+}
