ef63fcf6b9c5f4d55fc07d66f87f4a48d50462e3
Douglas Y. Yabusame <dyabusame@itelinfo.com>
2021-09-22 15:42:21 -0400

    inserindo repositorio CidadeModalidade

---
+++ src/main/java/com/comite/ComiteApp/repositories/CidadeModalidadeRepository.java
@@ -0,0 +1,8 @@
+package com.comite.ComiteApp.repositories;
+
+import com.comite.ComiteApp.entities.CidadeModalidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeModalidadeRepository extends JpaRepository<CidadeModalidade, Integer> {
+
+}
