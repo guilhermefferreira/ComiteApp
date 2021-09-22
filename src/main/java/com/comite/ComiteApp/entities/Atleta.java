5d51f93e126100e52db3cbab63caadfd52879e7e
Mateus Tomi <mtomi@itelinfo.com>
2021-09-22 15:23:42 -0400

    Entidade atleta criado

--- src/main/java/com/comite/ComiteApp/entities/Atleta.java
+++ src/main/java/com/comite/ComiteApp/entities/Atleta.java
@@ -1,6 +1,20 @@
 package com.comite.ComiteApp.entities;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "Atleta")
 public class Atleta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idatleta")
+    private int idatleta;
+
+    @Column(name = "nome", nullable = false, length = 50)
+    private String nome;
+
+    @Column(name = "inscricao", nullable = false, length = 50)
+    private String inscricao;
 
 }
