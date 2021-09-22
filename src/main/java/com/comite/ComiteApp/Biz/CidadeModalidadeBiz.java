9f313b0fdc3c69fc2b3279fd9cf4fb8eee2fc4e5
Douglas Y. Yabusame <dyabusame@itelinfo.com>
2021-09-22 17:04:31 -0400

    controller cidademodalidade

--- src/main/java/com/comite/ComiteApp/Biz/CidadeModalidadeBiz.java
+++ src/main/java/com/comite/ComiteApp/Biz/CidadeModalidadeBiz.java
@@ -1,12 +1,5 @@
 package com.comite.ComiteApp.Biz;
 
-import java.util.Set;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 import com.comite.ComiteApp.Mensagem;
 import com.comite.ComiteApp.entities.CidadeModalidade;
 import com.comite.ComiteApp.repositories.ModalidadeRepository;
@@ -17,9 +10,6 @@ public class CidadeModalidadeBiz {
 
     private CidadeRepository cidadeRepository;
     private ModalidadeRepository modalidadeRepository;
- 
-    private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-    private Validator validator = factory.getValidator();
 
     public CidadeModalidadeBiz(CidadeRepository cidadeRepository, ModalidadeRepository modalidadeRepository) {
         msg = new Mensagem();
@@ -28,9 +18,7 @@ public class CidadeModalidadeBiz {
     }
 
     public Boolean Validade(CidadeModalidade cidademodalidade) {
-
-        Set<ConstraintViolation<CidadeModalidade>> constraintViolationSet = validator.validate(cidademodalidade);
-
+       
         Boolean result = true;
       
         if (cidadeRepository.findById(cidademodalidade.getIdCidade()).isEmpty()) {
@@ -40,12 +28,7 @@ public class CidadeModalidadeBiz {
         if (modalidadeRepository.findById(cidademodalidade.getIdModalidade()).isEmpty()) {
             msg.mensagens.add("Id inexistente");
             result = false;
-        }     
-        if(!constraintViolationSet.isEmpty()){
-            ConstraintViolationException exceptionConstraint = new ConstraintViolationException(constraintViolationSet);
-            msg.mensagens.add(exceptionConstraint.getMessage());
-            result = false;
-        }
+        }   
         if(result == true){
             msg.mensagens.add("OK");
         }
