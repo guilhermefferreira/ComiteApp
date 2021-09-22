02f43c87e18e21b00494ed6ee8d39b2132512ab6
Guilherme F. Ferreira <gferreira@itelinfo.com>
2021-09-22 14:59:25 -0400

    Projeto

---
+++ src/main/java/com/comite/ComiteApp/ComiteAppApplication.java
@@ -0,0 +1,13 @@
+package com.comite.ComiteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComiteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComiteAppApplication.class, args);
+	}
+
+}
